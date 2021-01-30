3f97f64fd406a1b208dbf00375c1e652b1d7763b
Jorge Gutiérrez <jorgeedugutima15@gmail.com>
2021-01-30 17:39:26 -0600

    Dialogue update

--- Serendipia/Assets/Scripts/DialogoManager.cs
+++ Serendipia/Assets/Scripts/DialogoManager.cs
@@ -3,57 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//Base on https://www.youtube.com/watch?v=_nRzoTzeyxU and https://www.youtube.com/watch?v=f-oSXg6_AMQ taking the Kendall Tencio implementation.
+//Modified by Jorge Gutiérrez
 public class DialogoManager : MonoBehaviour
 {
-    public Text nameText;
-    public Text dialogueText;
+    private int index;
+    [Header("Text box objects")]
+    public GameObject text_box_canvas; //to active true or false
+    public Text name_Field_Obj;
+    public Text dialogue_Field_Obj;
+    public Image character_default_Image;
+    public Button dialogue_start_button;
+    [Header("Text box Data")]
+    public string character_Name;
+    public int[] character_talk_order;
+    [TextArea(3, 10)]
+    public string[] sentences;
+    //public string[] ownSentences;
     public float typingSpeed;
-    public Animator animator;
+    public Sprite[] character_Images;
 
-    private Queue<string> sentences;
-
-    // Start is called before the first frame update
-    void Start()
+    [Header("Animator")]
+    public Animator animator; 
+    
+    public void StartDialogue()
     {
-        sentences = new Queue<string>();
-    }
-
-    public void StartDialogue(Dialogo dialogue)
-    {
+        dialogue_start_button.gameObject.SetActive(false);
+        character_default_Image.gameObject.SetActive(true);
         animator.SetBool("isOpened", true);
-        nameText.text = dialogue.name;
-        sentences.Clear();
-
-        foreach (string sentece in dialogue.sentences)
-        {
-            sentences.Enqueue(sentece);
-        }
+        dialogue_Field_Obj.text = character_Name;                
         DisplayNextSentence();
     }
+
     public void DisplayNextSentence() { 
-        if (sentences.Count == 0) //osea que alcanzamos el final de la cola
+        if (index < sentences.Length) //osea que alcanzamos el final de la cola
+        {            
+            string sentence = sentences[index];
+            dialogue_Field_Obj.text = sentence;
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(sentence));
+            index++;
+        }
+        else
         {
+            dialogue_Field_Obj.text = "";            
             EndDialogo();
-            return;
+                        
         }        
-        string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
-        StopAllCoroutines();
-        StartCoroutine(TypeSentence(sentence));
     }
 
     IEnumerator TypeSentence(string sentence)
     {
-        dialogueText.text = "";
+        Debug.Log("index: " + index);
+        character_default_Image.sprite = character_Images[character_talk_order[index]];// asigno game object..
+        
+        /*
+         * if(index ==0){
+         * what_to_do_text.setActive(true);
+         * answer_one_button.setActive(true);
+         * answer_two_button.setActive(true);
+         * next_button.setActive(false);
+         * answer_one_button.text = response_one;
+         * answer_two_button.text = response_two;
+         * }else{
+         * * what_to_do_text.setActive(false);
+         * answer_one_button.setActive(false);
+         * answer_two_button.setActive(false);
+         * next_button.setActive(true);
+         * código de abajo
+         */
+        dialogue_Field_Obj.text = "";
         foreach(char letter in sentence.ToCharArray())
         {
-            dialogueText.text += letter;
+            dialogue_Field_Obj.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
     }
 
     void EndDialogo()
     {
+        character_default_Image.gameObject.SetActive(false);
         animator.SetBool("isOpened", false);
+        dialogue_start_button.gameObject.SetActive(true);
+        index = 0;
     }
 }
