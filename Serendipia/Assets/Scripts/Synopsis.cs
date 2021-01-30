34c3afe8e8c831b467d6d3f38df4b95f53c6bef1
Kendall Tencio Esquivel <kendalltencio132e@gmail.com>
2021-01-30 15:02:57 -0600

    Synopsis (Init) and Sprites

---
+++ Serendipia/Assets/Scripts/Synopsis.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Synopsis : MonoBehaviour
+{
+    public string[] text_sentences;
+    public GameObject[] images;
+    public GameObject images_holder;
+    public Text text_display;
+
+    public GameObject canvas;
+    public Text chapter_text;
+
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    IEnumerator FadeImage()
+    {
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            chapter_text.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1.5f);
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            chapter_text.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    void NextSentence()
+    {
+        canvas.SetActive(true);
+        StartCoroutine(FadeImage());
+    }
+}
